32073f788f709f8d790c99c2782571181d405a67
doande68 <doandinhde2k3@gmail.com>
2024-03-15 19:11:12 +0700

    hghg

---
+++ user/create1.js
@@ -0,0 +1,32 @@
+function showFormCreate1() {
+    document.getElementById('main').innerHTML = `
+    <div>
+    <input type="text" id="name" placeholder="Name">
+    <input type="text" id="address" placeholder="Address">
+    <input type="text" id="phoneNumber" placeholder="PhoneNumer">
+    <input type="text" id="email" placeholder="Email">
+    <input type="text" id="password" placeholder="PassWord">
+    <button onclick="create1()">Thêm mới</button>
+</div>
+    `
+}
+
+function create1() {
+    let name = document.getElementById('name').value;
+    let address = document.getElementById('address').value;
+    let phoneNumber = document.getElementById('phoneNumber').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    let newUser = {
+        name: name,
+        address: address,
+        phoneNumber: phoneNumber,
+        email: email,
+        password: password,
+    }
+
+    axios.post('http://localhost:8080/user/add', newUser).then(function (response) {
+        showHome1();
+    })
+}
